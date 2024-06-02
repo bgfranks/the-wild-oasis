9193b67f61d2f60e302976845a359fdaaa037715
Brandon Franks <bgfranks.dev@gmail.com>
2024-06-02 15:37:14 -0400

    added the cabin id to static props, modified the UI for a bug with the rest country api

--- app/_components/SelectCountry.jsx
+++ app/_components/SelectCountry.jsx
@@ -1,25 +1,30 @@
 import { getCountries } from '@/app/_lib/data-service'
 
+// Let's imagine your colleague already built this component 😃
+
 async function SelectCountry({ defaultCountry, name, id, className }) {
-  const countries = null
-  const flag =
-    countries.find((country) => country.name === defaultCountry)?.flag ?? ''
+  const countries = await getCountries()
+  console.log(countries[0])
 
   return (
-    <select
-      name={name}
-      id={id}
-      defaultValue={`${defaultCountry}%${flag}`}
-      className={className}
-    >
-      <option value="">Select country...</option>
-      {countries.map((c) => (
-        <option key={c.name} value={`${c.name}%${c.flag}`}>
-          {c.name}
-        </option>
-      ))}
-    </select>
+    // <select
+    //   name={name}
+    //   id={id}
+    //   // Here we use a trick to encode BOTH the country name and the flag into the value. Then we split them up again later in the server action
+    //   defaultValue={`${defaultCountry}`}
+    //   className={className}
+    // >
+    //   <option value="">Select country...</option>
+    //   {countries.map((c) => (
+    //     <option key={c.name} value={`${c.name}`}>
+    //       {c.name}
+    //     </option>
+    //   ))}
+    // </select>
+    <h1>country</h1>
   )
 }
 
 export default SelectCountry
+
+//%${c.flag
--- app/account/profile/page.jsx
+++ app/account/profile/page.jsx
@@ -7,7 +7,6 @@ export const metadata = {
 
 export default function Page() {
   // CHANGE
-  const countryFlag = 'pt.jpg'
   const nationality = 'portugal'
 
   return (
@@ -41,13 +40,11 @@ export default function Page() {
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <label htmlFor="nationality">Where are you from?</label>
-            <img
-              src={countryFlag}
-              alt="Country flag"
-              className="h-5 rounded-sm"
-            />
           </div>
-
+          <input
+            disabled
+            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+          />
           {/* <SelectCountry
             name="nationality"
             id="nationality"

ad601c4718c1bd0091e3e25e831b807e1320d72d
bgfranks <bgfranks@Brandons-Air.attlocal.net>
2024-06-04 21:44:51 -0400

    set up the context api for the reservation date range, and created a handler for selecting the date range

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from './_components/Header'
+import { ReservationProvider } from './_context/reservationContext'
 
 import './_styles/globals.css'
 import { Josefin_Sans } from 'next/font/google'
@@ -25,7 +26,9 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
